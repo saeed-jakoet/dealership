670ee0d9f9936cf143e19bbfa05ee711daf2bcd9
saeed-jakoet <jakoetsaeed@gmail.com>
2024-01-16 22:57:25 +0200

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,67 +3,89 @@
 import React from 'react';
 import { Link } from 'react-scroll';
 import { FaInstagram, FaFacebook, FaWhatsapp } from 'react-icons/fa';
-import Logo from '../images/logo4.png'
+import Logo from '../images/logo4.png';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className='container'>
-        <div className='columns'>
+    <div className="footer">
+      <div className="container">
+        <div className="columns">
           {/* Logo */}
-          <div className='column'>
-            <img src={Logo} alt='Logo' className='logo' />
+          <div className="column">
+            <img src={Logo} alt="Logo" className="logo" />
           </div>
 
           {/* Column 1: Navigation */}
-          <div className='column'>
+          <div className="column">
             <h3>Navigation</h3>
             <ul>
-              <li><Link to='hero' smooth={true} duration={500}>Home</Link></li>
-              <li><Link to='about' smooth={true} duration={500}>About</Link></li>
-              <li><Link to='listings' smooth={true} duration={500}>Showroom</Link></li>
-              <li><Link to='demo' smooth={true} duration={500}>Contact Us</Link></li>
+              <li>
+                <Link to="hero" smooth={true} duration={500}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="about" smooth={true} duration={500}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="listings" smooth={true} duration={500}>
+                  Showroom
+                </Link>
+              </li>
+              <li>
+                <Link to="demo" smooth={true} duration={500}>
+                  Contact Us
+                </Link>
+              </li>
             </ul>
           </div>
 
           {/* Column 2: Opening and Closing Times */}
-          <div className='column'>
+          <div className="column">
             <h3>Opening Hours</h3>
-            <p>Monday - Friday: 08:30 - 17:00</p>
-            <p>Saturday: 09:00  - 13:00 </p>
-            <p>Sunday & public holidays: On Appointment</p>
+            <p>Monday - Thursday: 09:00 - 16:00</p>
+            <p>Friday: 09:00 - 12:00/14:00 - 16:00</p>
+            <p>Saturday: Appointment Only</p>
+            <p>Public holidays: Appointment Only</p>
           </div>
 
           {/* Column 3: Contact Information */}
-          <div className='column'>
+          <div className="column">
             <h3>Contact Information</h3>
             <p>Email: info@farauto.com</p>
             <p>Phone: +27 83 578 9437</p>
-            <p>Address: 3 Muir Street and Selkirk Street District 6 Zonnebloem Cape Town South Africa</p>
+            <p>Address: 3 Muir Street, Zonnebloem, 8001</p>
           </div>
         </div>
 
-        <div className='social-icons-footer'>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-            <FaFacebook />
-          </a>
-          <a href="https://www.instagram.com/far.auto_/" target="_blank" rel="noopener noreferrer">
+        <div className="social-icons-footer">
+          <a
+            href="https://www.instagram.com/far.auto_/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaInstagram />
           </a>
-          <a href="https://wa.me/+27718740886" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://wa.me/+27718740886"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaWhatsapp />
           </a>
         </div>
 
-        <div className='bottom'>
-          <span className='line'></span>
+        <div className="bottom">
+          <span className="line"></span>
           <p>2021 FARAUTO. ALL RIGHTS RESERVED</p>
           <p>Powered by: RAMAF (PTY) LTD</p>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -7,216 +7,239 @@ import { carsData } from '../carsData';
 import ImageCarousel from '../ImageCarousel';
 import EnquiryForm from './EnquiryForm';
 import { MdEvent, MdLocalGasStation, MdSpeed } from 'react-icons/md';
-import { TbManualGearbox } from "react-icons/tb";
+import { TbManualGearbox } from 'react-icons/tb';
 
 import noCarPhoto from '../images/nophotocar.jpg';
 
 const Listings = () => {
-    const carsPerPage = 4;
-    const [selectedCar, setSelectedCar] = useState(null);
-    const [showEnquiryForm, setShowEnquiryForm] = useState(false);
-    const [disableScroll, setDisableScroll] = useState(false);
-    const [page, setPage] = useState(1);
+  const carsPerPage = 4;
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [showEnquiryForm, setShowEnquiryForm] = useState(false);
+  const [disableScroll, setDisableScroll] = useState(false);
+  const [page, setPage] = useState(1);
 
-    const listingsRef = useRef(null);
+  const listingsRef = useRef(null);
 
-    const handleListingClick = (car) => {
-        setSelectedCar(car);
-        setShowEnquiryForm(false);
-        setDisableScroll(true);
-    };
+  const handleListingClick = (car) => {
+    setSelectedCar(car);
+    setShowEnquiryForm(false);
+    setDisableScroll(true);
+  };
 
-    const handleCloseModal = () => {
-        setSelectedCar(null);
-        setShowEnquiryForm(false);
-        setDisableScroll(false);
-    };
+  const handleCloseModal = () => {
+    setSelectedCar(null);
+    setShowEnquiryForm(false);
+    setDisableScroll(false);
+  };
 
-const handlePageChange = (direction) => {
+  const handlePageChange = (direction) => {
     const listingsOffsetTop = listingsRef.current.offsetTop;
 
     if (direction === 'prev' && page > 1) {
-        window.scrollTo({ top: listingsOffsetTop, behavior: 'smooth' });
-        setTimeout(() => {
-            setPage(page - 1);
-        }, 500); // Delay setting the page by 500 milliseconds
-    } else if (direction === 'next' && page < Math.ceil(carsData.length / carsPerPage)) {
-        window.scrollTo({ top: listingsOffsetTop, behavior: 'smooth' });
-        setTimeout(() => {
-            setPage(page + 1);
-        }, 500); // Delay setting the page by 500 milliseconds
+      window.scrollTo({ top: listingsOffsetTop, behavior: 'smooth' });
+      setTimeout(() => {
+        setPage(page - 1);
+      }, 500); // Delay setting the page by 500 milliseconds
+    } else if (
+      direction === 'next' &&
+      page < Math.ceil(carsData.length / carsPerPage)
+    ) {
+      window.scrollTo({ top: listingsOffsetTop, behavior: 'smooth' });
+      setTimeout(() => {
+        setPage(page + 1);
+      }, 500); // Delay setting the page by 500 milliseconds
     }
-};
- 
-    
-    useEffect(() => {
-        
-        const handleBodyScroll = () => {
-            if (disableScroll) {
-                document.body.style.overflow = 'hidden';
-            } else {
-                document.body.style.overflow = 'auto';
-            }
-        };
+  };
 
-        // Add event listener for scroll
-        window.addEventListener('scroll', handleBodyScroll);
-
-        // Remove the event listener when the component unmounts
-        return () => {
-            window.removeEventListener('scroll', handleBodyScroll);
-            // Reset overflow property when component unmounts
-            document.body.style.overflow = 'auto';
-        };
-    }, [disableScroll]);
-
-    useEffect(() => {
-        if (listingsRef.current && page > 1) {
-            listingsRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    }, [page]);
-
-    const handleFormButtonClick = (e) => {
-        e.stopPropagation();
-        setShowEnquiryForm(true);
-        setDisableScroll(true);
+  useEffect(() => {
+    const handleBodyScroll = () => {
+      if (disableScroll) {
+        document.body.style.overflow = 'hidden';
+      } else {
+        document.body.style.overflow = 'auto';
+      }
     };
 
-    const handleModalClick = (e) => {
-        e.stopPropagation();
+    // Add event listener for scroll
+    window.addEventListener('scroll', handleBodyScroll);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleBodyScroll);
+      // Reset overflow property when component unmounts
+      document.body.style.overflow = 'auto';
     };
+  }, [disableScroll]);
 
-    const filteredCars = carsData
-        .sort((a, b) => a.brand.localeCompare(b.brand)) // Sort alphabetically by brand
-        .slice((page - 1) * carsPerPage, page * carsPerPage); // Paginate cars
+  useEffect(() => {
+    if (listingsRef.current && page > 1) {
+      listingsRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [page]);
 
-    return (
-        <Element name='listings'>
-            <div className="listings-container" ref={listingsRef}>
-                <div className="listings-header">
-                    <h1>Car Listings</h1>
-                    <p>Explore our latest car inventory</p>
-                </div>
-                {filteredCars.map((vehicle, index) => (
-                    <ListingItem
-                        key={index}
-                        vehicle={vehicle}
-                        onClick={() => handleListingClick(vehicle)}
-                        onClose={handleCloseModal}
-                        onEnquireClick={handleFormButtonClick}
+  const handleFormButtonClick = (e) => {
+    e.stopPropagation();
+    setShowEnquiryForm(true);
+    setDisableScroll(true);
+  };
+
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
+
+  const filteredCars = carsData
+    .sort((a, b) => a.brand.localeCompare(b.brand)) // Sort alphabetically by brand
+    .slice((page - 1) * carsPerPage, page * carsPerPage); // Paginate cars
+
+  return (
+    <Element name="listings">
+      <div className="listings-container" ref={listingsRef}>
+        <Banner
+          page={page}
+          totalPages={Math.ceil(carsData.length / carsPerPage)}
+          onPageChange={handlePageChange}
+        />
+        <div className="listings-header">
+          <h1>Car Listings</h1>
+          <p>Explore our latest car inventory</p>
+        </div>
+        {filteredCars.map((vehicle, index) => (
+          <ListingItem
+            key={index}
+            vehicle={vehicle}
+            onClick={() => handleListingClick(vehicle)}
+            onClose={handleCloseModal}
+            onEnquireClick={handleFormButtonClick}
+          />
+        ))}
+        {selectedCar && selectedCar.imageUrls && (
+          <div className="modal-overlay" onClick={handleCloseModal}>
+            <div className="modal-content" onClick={handleModalClick}>
+              <ImageCarousel
+                carDetails={selectedCar}
+                onClose={handleCloseModal}
+              />
+              <div className="enquiry-form-container">
+                {showEnquiryForm && selectedCar && (
+                  <motion.div
+                    initial={{ opacity: 0, y: -30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: -30 }}
+                  >
+                    <EnquiryForm
+                      vehicle={selectedCar}
+                      onClose={() => setShowEnquiryForm(false)}
+                      isFormVisible={showEnquiryForm}
                     />
-                ))}
-                {selectedCar && selectedCar.imageUrls && (
-                    <div className="modal-overlay" onClick={handleCloseModal}>
-                        <div className="modal-content" onClick={handleModalClick}>
-                            <ImageCarousel carDetails={selectedCar} onClose={handleCloseModal} />
-                            <div className="enquiry-form-container">
-                                {showEnquiryForm && selectedCar && (
-                                    <motion.div
-                                        initial={{ opacity: 0, y: -30 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        exit={{ opacity: 0, y: -30 }}
-                                    >
-                                        <EnquiryForm
-                                            vehicle={selectedCar}
-                                            onClose={() => setShowEnquiryForm(false)}
-                                            isFormVisible={showEnquiryForm}
-                                        />
-                                    </motion.div>
-                                )}
-                                {!showEnquiryForm && (
-                                    <motion.button
-                                        className="enquire-button"
-                                        onClick={handleFormButtonClick}
-                                    >
-                                        Enquire
-                                    </motion.button>
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                  </motion.div>
                 )}
-                <Banner page={page} totalPages={Math.ceil(carsData.length / carsPerPage)} onPageChange={handlePageChange} />
+                {!showEnquiryForm && (
+                  <motion.button
+                    className="enquire-button"
+                    onClick={handleFormButtonClick}
+                  >
+                    Enquire
+                  </motion.button>
+                )}
+              </div>
             </div>
-        </Element>
-    );
+          </div>
+        )}
+        <Banner
+          page={page}
+          totalPages={Math.ceil(carsData.length / carsPerPage)}
+          onPageChange={handlePageChange}
+        />
+      </div>
+    </Element>
+  );
 };
 
-const noCarPhotoImage = <img src={noCarPhoto} alt="No Car Available" className="icon" />;
+const noCarPhotoImage = (
+  <img src={noCarPhoto} alt="No Car Available" className="icon" />
+);
 
 const ListingItem = ({ vehicle, onClick, onClose, onEnquireClick }) => {
-    const controls = useAnimation();
-    const [ref, inView] = useInView({
-        triggerOnce: true,
-        rootMargin: '-100px 0px',
-    });
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    rootMargin: '-100px 0px',
+  });
 
-    useEffect(() => {
-        if (inView) {
-            controls.start({
-                opacity: 1,
-                y: 0,
-                transition: { duration: 0.5 },
-            });
-        }
-    }, [controls, inView]);
+  useEffect(() => {
+    if (inView) {
+      controls.start({
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.5 },
+      });
+    }
+  }, [controls, inView]);
 
-    return (
-        <motion.div
-            ref={ref}
-            className="vehicle-listing"
-            onClick={onClick}
-            initial={{ opacity: 0, y: 100 }}
-            animate={controls}
-        >
-            <div className='image-container'>
-                {vehicle.imageUrls && vehicle.imageUrls.length > 0 ? (
-                    <img src={vehicle.imageUrls[0]} alt={`${vehicle.name} Vehicle`} />
-                ) : (
-                    noCarPhotoImage
-                )}
-            </div>
-            <div className='details-container'>
-                <div className='top-details'>
-                    <p className='price'>{`R${vehicle.price.toLocaleString()}`}</p>
-                </div>
-                <div className='bottom-details'>
-                    <p className='name'>{vehicle.name}</p>
-                    <p className='used-status'>{vehicle.used ? 'Used Car' : 'New Car'}</p>
-                    <p className='mileage'>
-                        <MdSpeed className="icon" /> {/* React icon for mileage */}
-                        {vehicle.mileage} km
-                    </p>
-                    <p className='transmission-type'>
-                    <TbManualGearbox className="icon" /> {/* React icon for transmission */}
-                        {vehicle.transmissionType}
-                    </p>
-                    <p className='fuel-type'>
-                        <MdLocalGasStation className="icon" /> {/* React icon for fuel */}
-                        {vehicle.fuelType}
-                    </p>
-                    <p className='calendar'>
-                        <MdEvent className="icon" /> {/* React icon for calendar */}
-                        {vehicle.year}
-                    </p>
-                </div>
-            </div>
-        </motion.div>
-    );
+  return (
+    <motion.div
+      ref={ref}
+      className="vehicle-listing"
+      onClick={onClick}
+      initial={{ opacity: 0, y: 100 }}
+      animate={controls}
+    >
+      <div className="image-container">
+        {vehicle.imageUrls && vehicle.imageUrls.length > 0 ? (
+          <img src={vehicle.imageUrls[0]} alt={`${vehicle.name} Vehicle`} />
+        ) : (
+          noCarPhotoImage
+        )}
+      </div>
+      <div className="details-container">
+        <div className="top-details">
+          <p className="price">{`R${vehicle.price.toLocaleString()}`}</p>
+        </div>
+        <div className="bottom-details">
+          <p className="name">{vehicle.name}</p>
+          <p className="used-status">{vehicle.used ? 'Used Car' : 'New Car'}</p>
+          <p className="mileage">
+            <MdSpeed className="icon" />
+            {vehicle.mileage} km
+          </p>
+          <p className="transmission-type">
+            <TbManualGearbox className="icon" />
+            {vehicle.transmissionType}
+          </p>
+          <p className="fuel-type">
+            <MdLocalGasStation className="icon" />
+            {vehicle.fuelType}
+          </p>
+          <p className="calendar">
+            <MdEvent className="icon" />
+            {vehicle.year}
+          </p>
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 const Banner = ({ page, totalPages, onPageChange }) => {
-    return (
-        <div className="banner-container">
-            <button className="arrow-button" onClick={() => onPageChange('prev')} disabled={page === 1}>
-                {'<'}
-            </button>
-            <p className="page-indicator">{`Page ${page} of ${totalPages}`}</p>
-            <button className="arrow-button" onClick={() => onPageChange('next')} disabled={page === totalPages}>
-                {'>'}
-            </button>
-        </div>
-    );
+  return (
+    <div className="banner-container">
+      <button
+        className="arrow-button"
+        onClick={() => onPageChange('prev')}
+        disabled={page === 1}
+      >
+        {'<'}
+      </button>
+      <p className="page-indicator">{`Page ${page} of ${totalPages}`}</p>
+      <button
+        className="arrow-button"
+        onClick={() => onPageChange('next')}
+        disabled={page === totalPages}
+      >
+        {'>'}
+      </button>
+    </div>
+  );
 };
 
 export default Listings;
