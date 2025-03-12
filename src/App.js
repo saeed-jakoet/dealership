a51219d56c59d196106b1ee9f6b9788ebb0c2007
saeed-jakoet <jakoetsaeed@gmail.com>
2025-03-12 14:26:27 +0200

    under construction page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import UnderConstruction from './components/UnderConstruction/UnderConstruction';
 import Navbar from './components/Navbar/Navbar';
 import Page from './components/Page';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Page content="hero" />
-      <Page content="about" />
-      {/* <Page content="featured" /> */}
-      <Page content="listings" />
-      <Page content="contact" />
-      <Page content="reviews" />
-      <Footer />
-    </Router>
-  );
+    const isUnderConstruction = true; // Set this flag to true to show the under construction page
+
+    return (
+        <Router>
+            {isUnderConstruction ? (
+                <UnderConstruction />
+            ) : (
+                <>
+                    <Navbar />
+                    <Page content="hero" />
+                    <Page content="about" />
+                    <Page content="listings" />
+                    <Page content="contact" />
+                    <Page content="reviews" />
+                    <Footer />
+                </>
+            )}
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./UnderConstruction.css";
+
+const UnderConstruction = () => {
+    return (
+        <div className="under-construction">
+            <h1 className="glitch">Under Construction</h1>
+            <p>We're working on something awesome. Stay tuned!</p>
+            <div className="loading-bar"></div>
+        </div>
+    );
+};
+
+export default UnderConstruction;
